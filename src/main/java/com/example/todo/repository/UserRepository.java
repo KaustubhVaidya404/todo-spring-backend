c1061aa519180e883cd50d033e3126d31001f1de
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2024-10-19 14:21:50 +0530

    initial commit

---
+++ src/main/java/com/example/todo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+
+package com.example.todo.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.todo.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+	User findByEmail(String email);
+}
---
+++ src/test/java/com/example/todo/repository/TodoUserRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.example.todo.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.example.todo.models.User;
+
+@DataJpaTest
+public class TodoUserRepositoryTest {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	@DisplayName("Test for saving new user")
+	public void testSaveUser() {
+		
+		User user = new User();
+		user.setName("Test User1");
+		user.setEmail("testemail1@email.com");
+		user.setPassword("test_password_1");
+		
+		User savedUser = userRepository.save(user);
+		
+		assertThat(savedUser.getId()).isNotNull();
+		assertThat(savedUser.getName()).isEqualTo("Test User1");
+		assertThat(savedUser.getEmail()).isEqualTo("testemail1@email.com");
+		
+	}
+	
+	@Test
+	@DisplayName("Test for retriving name from email address")
+	public void testGetNameByEmail() {
+		
+		User getUser = userRepository.findByEmail("testemail1@email.com");
+		
+		assertThat(getUser.getName()).isEqualTo("Test User1");
+		
+	}
+	
+	@Test
+	@DisplayName("Test for deleting the user")
+	public void testDeleteUser() {
+		
+	}
+}
